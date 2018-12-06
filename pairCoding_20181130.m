f30de45152fd5e4cf9a4135bfc6744a3fd1a30e7
Adree Songco Aguas <adree@uw.edu>
2018-12-06 15:10:35 -0800

    Add matrix compatibility and var explained calculation

--- pairCoding_20181130.m
+++ pairCoding_20181130.m
@@ -1,15 +1,17 @@
 %% LN modeling skeleton
 clear;
 
-%Set the parameters used to derive the model
+%TODO: Retrieve the four parameters below from metadata automatically 
+%before exporting as .mat file
 params.samplingInterval = 1e-4;
 params.frequencyCutoff = 20;
-params.staPts = 25000;
-params.computeNL = 1;
-params.numBins = 40;
-
-params.prePts = 3000;  % TODO: retrieve pre- and stim-points in a better way
+params.prePts = 3000;  
 params.stimPts = 50000;
+
+%TODO: Figure out how to use staPts + truncating the filter effectively
+params.staPts = 25000;
+
+params.numBins = 40; %Is it fine if this is fixed?
 
 %% get data
 fileName = '041317Ac3.mat';
@@ -20,51 +22,68 @@ stimComplete = S.BlueStimuliExc;
 % Baseline correction, filtering, etc
 %   - note these steps do not affect the the output of getFilter, assuming constant frequency cutoff
 responseComplete = ApplyFrequencyCutoffOvation(responseComplete, params.frequencyCutoff, params.samplingInterval);
-responseComplete = BaselineCorrectOvation(responseComplete, 1, params.prePts); % TODO: make version that finds prepoints automatically
+responseComplete = BaselineCorrectOvation(responseComplete, 1, params.prePts);
 
 response = trimPreAndTailPts(responseComplete, params.prePts, params.stimPts);
 stim = trimPreAndTailPts(stimComplete, params.prePts, params.stimPts);
 
 %% get model
 filter = getFilter(stim, response, params);
+midpoint = round(length(filter)/2); %TODO: truncate the filter in the filter collection stage
+%filter(midpoint:end) = 0;
+midpoint = round(length(filter)/2);
+filter = [filter(1, midpoint + 1:end) filter(1, 1:midpoint)];
 figure; plot(filter);
 
-%midpoint = round(length(filter)/2);
-%filter = [filter(1, midpoint + 1:end) filter(1, 1:midpoint)];
-%figure; plot(filter);
+figure; plot(filter);
 
 rawNL = getRawNL(filter, response, stim, params); 
-figure; plot(rawNL(1,:), rawNL(2,:));
 
 degree = 3; 
 fitNL = getPolynomialFitNL(rawNL, degree);
 evalPoly = polyval(fitNL.coeff, rawNL(1,:), [], fitNL.mu);
+
+figure; plot(rawNL(1,:), rawNL(2,:));
 hold on; plot(rawNL(1,:), evalPoly);
 
 %% forward run
-% prediction, generatorSignal = getPrediction(filter, fitNL, stim)
 prediction = getPrediction(filter, stim, @polyval, fitNL);
-figure; plot(prediction(1,:));
-hold on; plot(response(1,:))
 
 %% evaluate performance
-% rSquared = getVarExplainedLN(prediction, data)
+[rSquared, meanRSquared] = getVarExplainedLN(prediction, response);
 
+figure; plot(rSquared);
+hold on; plot(meanRSquared .* ones(1,length(rSquared)));
 
 %% local functions
 
+function [rSquared, meanRSquared] = getVarExplainedLN(prediction, response)
+
+epochCount = size(prediction, 1);
+rSquared = ones(1,epochCount);
+
+for ii = 1:epochCount %TODO: Set this as vectorized 
+    singleResponse = response(ii,:);
+    singlePrediction = prediction(ii,:);
+    responseMean = mean(singleResponse,2);
+    
+    sumSquareErr = singleResponse - singlePrediction;
+    sumSquareTotal = singleResponse - responseMean;
+    
+    sumSquareErr = sum(sumSquareErr.^2);
+    sumSquareTotal = sum(sumSquareTotal.^2);
+    
+    rSquared(1,ii) = 1 - (sumSquareErr / sumSquareTotal);
+    
+end
+
+meanRSquared = mean(rSquared);
+
+end
+
 function prediction = getPrediction(filter, stim, nlFn, fnParams)
 
-numEpochs = size(stim, 1);
-numPts = size(stim, 2);
-
-% Generate a straight line with params.numBins points between the largest
-% and smallest generator signal.
-generatorSignal = zeros(numEpochs, numPts);
-for ii = 1:numEpochs
-    stimSingle = stim(ii, :); %- mean(stim(ii, :));  % subtract mean from stim
-    generatorSignal(ii,:) = convolveFilterWithStim(filter, stimSingle);
-end
+generatorSignal = convolveFilterWithStim(filter, stim);
 
 if isequal(nlFn, @polyval)
     prediction = nlFn(fnParams.coeff, generatorSignal, [], fnParams.mu);
@@ -76,17 +95,17 @@ end
 
 end
 
-
 function filter = getFilter(stim, response, params)
 numPts = size(stim, 2);
 
 % loop across all epochs to compute filter
 for ii = 1:size(response, 1)
     responseSingle = response(ii, :);
-    stimSingle = stim(ii, :) - mean(stim(ii, :));
+    stimSingle = stim(ii, :);
     
     dataFFT = fft(responseSingle);
     stmFFT = fft(stimSingle);
+    
     if ii == 1
         filterFFT = conj(stmFFT) .* dataFFT;
     else
@@ -110,19 +129,12 @@ filter = filter / max(abs(filter));  % normalize (set peak to 1)
 end
 
 function rawNL = getRawNL(filter, data, stim, params)
-numEpochs = size(stim, 1);
-numPts = size(stim, 2);
 
-% Generate a straight line with params.numBins points between the largest
-% and smallest generator signal.
-generatorSignal = zeros(numEpochs, numPts);
-for ii = 1:numEpochs
-    stimSingle = stim(ii, :); %- mean(stim(ii, :));  % subtract mean from stim
-    generatorSignal(ii,:) = convolveFilterWithStim(filter, stimSingle);
-end
+generatorSignal = convolveFilterWithStim(filter, stim);
+
 minGen = min(min(generatorSignal));
 maxGen = max(max(generatorSignal));
-step = (maxGen-minGen)/params.numBins;
+step = (maxGen-minGen) / params.numBins;
 
 bins = zeros(params.numBins, 1);
 for ii = 1:params.numBins
@@ -132,6 +144,8 @@ end
 % loop across all epochs to compute nonlinearity
 epochBinCnt = zeros(params.numBins,1);
 dataBinned = zeros(params.numBins,1);
+numEpochs = size(stim, 1);
+
 for ii = 1:numEpochs
     singleGeneratorSignal = generatorSignal(ii, :);
     binIndices = cell(params.numBins, 1);
@@ -146,32 +160,43 @@ for ii = 1:numEpochs
             if ii == 1
                 dataBinned(jj) = mean(singleEpochData(binIndices{jj}));
             else
-                dataBinned(jj) = dataBinned(jj) + mean(singleEpochData(binIndices{jj}));
+                dataBinned(jj) = dataBinned(jj) + ...
+                    mean(singleEpochData(binIndices{jj}));
             end
             epochBinCnt(jj) = epochBinCnt(jj) + 1;
         end
-    end
+    end 
     
 end
 
 dataBinned(epochBinCnt == 0) = NaN;
-% indices = find(epochBinCnt > 0);  %TODO: see if anything here is necessary when something is NaN
+dataBinned = dataBinned ./ epochBinCnt;
+if sum(isnan(dataBinned)) > 0
+    error('Bin contains zero counts! See lines 177-184.');
+end
+%The following lines might be useful if you get the error above.
+% indices = find(epochBinCnt > 0);  
 % dataBinned(indices) = dataBinned(indices) ./ epochBinCnt(indices);
+
 dataBinned = dataBinned ./ epochBinCnt;
 
-% fit nonlinearity and make full prediction
-nanLocs = isnan(dataBinned);
-indices = find(nanLocs == 0);
+%nanLocs = isnan(dataBinned);
+%indices = find(nanLocs == 0);
+
 rawNL(1,:) = bins;
 rawNL(2,:) = dataBinned;
 end
 
 function generatorSignal = convolveFilterWithStim(filter, stim)
 
-if length(filter) ~= length(stim)
-    diff = abs(length(filter) - length(stim));
+filterLength = size(filter,2);
+numPts = size(stim,2); 
+numEpochs = size(stim,1);
+
+if filterLength ~= numPts
+    diff = abs(filterLength - numPts);
     
-    %finds out if the length divided by two is even or odd
+    %finds out if the length of the difference is even or odd
     if mod(diff,2) ~= 0
         interval(1) = round(diff/2);
         interval(2) = round(diff/2) - 1;
@@ -180,9 +205,12 @@ if length(filter) ~= length(stim)
         interval(2) = interval(1);
     end
     
-    midpoint = round(length(filter)/2);
+    midpoint = round(filterLength / 2);
     
-    if length(filter) > length(stim)
+    %remove zeros from filter if stimuli length is shorter; otherwise, pad
+    %with zeros - in either case, start modification from the middle of
+    %filter
+    if filterLength > numPts
     	filter(midpoint-interval(1)+1:midpoint+interval(2)) = [];
     else
     	filter = [filter(1:midpoint) zeros(1,diff-1) filter(midpoint:end)];
@@ -190,9 +218,16 @@ if length(filter) ~= length(stim)
 end
 
 filterFFT = fft(filter);
-stimuliFFT = fft(stim);
-generatorSignalFFT = filterFFT .* stimuliFFT;
-generatorSignal = real(ifft(generatorSignalFFT));
+
+generatorSignalFFT = zeros(numEpochs, numPts);
+generatorSignal = zeros(numEpochs, numPts);
+
+for ii = 1:numEpochs
+    stimSingle = stim(ii,:);
+    stimSingleFFT = fft(stimSingle);
+    generatorSignalFFT(ii,:) = filterFFT .* stimSingleFFT;
+    generatorSignal(ii,:) = real(ifft(generatorSignalFFT(ii,:))); 
+end
 end
 
 function trimmed = trimPreAndTailPts(signal, prePts, stimPts)
